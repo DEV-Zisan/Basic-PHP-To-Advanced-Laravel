b1915edfd05e8ee9b2e7b3c79d76267ffc636300
DEV-Zisan <ujjolahmed275@gmail.com>
2022-05-20 17:37:38 +0600

    https://github.com/DEV-Zisan/class-4.git

---
+++ Class4/index.php
@@ -0,0 +1,28 @@
+<?php
+//For loop ar bebohar
+for ( $i = 1; $i <= 10; $i++) {
+    echo "Ami deri kore Asbo";
+    echo "<br>";
+}
+echo " <br>";
+//while loop ar bebohar {for loop ar modde bariavle ta inline a likte hoy ar whil loop a bariable ta age assign kore dite hoy}
+ 
+$i = 1;
+while ( $i <= 10) {
+    echo "ami deri kore asbo na";
+    echo " <br>";
+    $i++;
+}
+
+//do while loop ar bebohar {for loop ar ar motoi tobe atar bises boisis to holo tru or fals hok gotona akbar gotai felbe}
+echo " <br>";
+$i = 1;
+do {
+    echo "ami akhon programer hoye gesi";
+    echo " <br>";
+    $i++;
+} while ($i <=10);
+
+
+ 
+?>
